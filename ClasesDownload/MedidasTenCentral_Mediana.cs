ef12bc60b64f1e61dac4eba8acea300ef615859f
Maribel Barrera <1018marybarrera@gmail.com>
2018-03-04 00:34:13 -0600

    traduccion de comentarios en ingles

--- ClasesDownload/MedidasTenCentral_Mediana.cs
+++ ClasesDownload/MedidasTenCentral_Mediana.cs
@@ -1,15 +1,30 @@
 using System;
 
+//ESPAÑOL: Abre la clase que calcula la mediana
+//ENGLISH: open the class that calculate the median value 
 public class mediana_JW
-{//Abre la clase que calcula la mediana
+{
+	//ESPAÑOL: El método recibe un arreglo de double's y devuelve un solo valor double
+	//ENGLISH: the method get a double array and return a value
+   	public static double calc_mediana(int n, double[] temp)
+   {
+   		//ESPAÑOL: Inicia método que calcula el promedio |  steps to calculate the average
 
-   public static double calc_mediana(int n, double[] temp)//El método recibe un arreglo de double's y devuelve un solo valor double
-   {//Inicia método que calcula el promedio
-      double mediana;//Declara variable para almacenar mediana con arreglo ordenado
-      if (n % 2 == 0)//if-else que aplica la operación para cantidad de valores pares o impares
-         mediana = (temp[n / 2] + temp[n / 2 - 1]) / 2;//Asigna valor de la mediana para arreglo de pares
-      else//else aplica la operación en valores impares
-         mediana = (temp[(n - 1) / 2]);//Asigna valor de la mediana para arreglo de impares                                    
-      return mediana;//Devuelve la mediana o valor de enmedio
-   }//Termina método que calcula la mediana
-}//Cierra la clase que calcula la mediana
+   		//ESPAÑOL: Declara variable para almacenar mediana con arreglo ordenado
+   		//ENGLISH: declare a variable to store the sorted array 
+      	double mediana;
+
+      	//ESPAÑOL: if-else que aplica la operación para cantidad de valores pares o impares
+      	// ENGLISH: check if the number of elements are even or odd
+      	if (n % 2 == 0) 
+      		//ESPAÑOL: Asigna valor de la mediana para arreglo de pares |ENGLISH: calculate the average for even array
+        	mediana = (temp[n / 2] + temp[n / 2 - 1]) / 2;
+
+        //ESPAÑOL: else aplica la operación en valores impares | ENGLISH: aply when the number of elements are odd
+      	else
+      		//ESPAÑOL: Asigna valor de la mediana para arreglo de impares | ENGLISH: calculate the average for odd array
+        	mediana = (temp[(n - 1) / 2]);                              
+      	return mediana;//ESPAÑOL: Devuelve la mediana o valor de enmedio | ENGLISH: return the median value
+
+   }//ESPAÑOL: Termina método que calcula la mediana | ENGLISH:
+}//ESPAÑOL: Cierra la clase que calcula la mediana | ENGLISH:
--- ClasesDownload/MedidasTenCentral_Moda.cs
+++ ClasesDownload/MedidasTenCentral_Moda.cs
@@ -1,55 +1,87 @@
 using System;
 
 public class moda_JW
-{//Inicia la clase moda_JW
+{
+   //ESPAÑOL: Inicia la clase moda_JW | ENGLISH: start the class that calculate the mode value
         
-   public static string calc_moda(int n, double[] temp)//El método recibe un arreglo de double's y devuelve un string o cadena
-   {//Inicia método que calcula la moda
+   //ESPAÑOL: El método recibe un arreglo de double's y devuelve un string o cadena
+   //ENGLISH: the method get a double array and return a string
+   public static string calc_moda(int n, double[] temp)
+   {
+      //ESPAÑOL: Inicia método que calcula la moda | ENGLISH: start  the method that calculate the mode
 
-      string sCadenaModa = "";//Declara e incializa la cadena para almacenar mediana con arreglo ordenado
-      string s1 = "", s2 = "", sTemp = "";//Declara e incializa las tres cadenas a emplear cuando los valores no ocurran una sola vez
-      int repeticiones_moda = 0;//Declara contador de repeticiones de moda
-      int i, j;//Subíndices locales
-      int m;//Tamaño de los arreglos auxiliares de empates y empates acumulados
-      m = n - 1;//Definición del tamaño de los arreglos auxiliares          
-      int[] emp = new int[m];//Declara el arreglo emp para guardar las banderas de empates entre valores
-      int[] emp_acum = new int[m];//Declara el arreglo emp_acum que contara cuantas repeticiones tiene un empate
-      int cont = 0;//Declara contador para cantidad de empates en emp_acum
-      int subindice_moda;//Declara subíndice de moda
-      int sum;//Declara el acumulador de sumatoria
+      //ESPAÑOL: Declara e incializa la cadena para almacenar mediana con arreglo ordenado 
+      //ENGLISH: variable to store the median sorted array 
+      string sCadenaModa = "";
 
+      //ESPAÑOL: Declara e incializa las tres cadenas a emplear cuando los valores no ocurran una sola vez
+      //ENGLISH: variables to save the values when it doesn't  happen once 
+      string s1 = "", s2 = "", sTemp = "";
+
+      //ESPAÑOL: Declara contador de repeticiones de moda |  ENGLISH: declare a counter for the repetitions
+      int repeticiones_moda = 0;
+      
+      int i, j;//ESPAÑOL: Subíndices locales | ENGLISH: local index
+      
+      int m;//ESPAÑOL: Tamaño de los arreglos auxiliares de empates y empates acumulados | ENGLISH: sizes for the arrays
+      
+      m = n - 1;//ESPAÑOL: Definición del tamaño de los arreglos auxiliares | ENGLISH: define the size for the auxiliar array     
+      
+      //ESPAÑOL: Declara el arreglo emp para guardar las banderas de empates entre valores 
+      //ENGLISH: array to save equal values 
+      int[] emp = new int[m];
+      
+      //ESPAÑOL: Declara el arreglo emp_acum que contara cuantas repeticiones tiene un empate 
+      //ENGLISH: declare an array to count the repetitions 
+      int[] emp_acum = new int[m];
+      
+      int cont = 0;//ESPAÑOL: Declara contador para cantidad de empates en emp_acum  | ENGLISH: declare a counter
+      
+      int subindice_moda;//ESPAÑOL: Declara subíndice de moda | ENGLISH: declare a mode index
+      
+      int sum;//ESPAÑOL: Declara el acumulador de sumatoria | ENGLISH: declare a store of summation
+
+      //ESPAÑOL: for de j que recorre todos los valores hasta m 
+      //ENGLISH: for loop to initialize arrays
       for (j = 0; j < m; j++)
-      {//for de j que recorre todos los valores hasta m
-         emp[j] = 0;//Inicializa los valores del arreglo de banderas de empates emp
-         emp_acum[j] = 0;//Inicializa los valores del arreglo de acumuladores de empates emp_acum
-      }//Cierra for de j que inicializa los dos arreglos
+      {
+         emp[j] = 0;//ESPAÑOL: Inicializa los valores del arreglo de banderas de empates emp 
+         emp_acum[j] = 0;//ESPAÑOL: Inicializa los valores del arreglo de acumuladores de empates emp_acum
+      }//ESPAÑOL: Cierra for de j que inicializa los dos arreglos 
 
+      //ESPAÑOL: Abre ciclo for de i que recorre todos los valores hasta n 
+      //ENGLISH: for loop to check both arrays  
       for (i = 0; i < n; i++)
-      {//Abre ciclo for de i que recorre todos los valores hasta n
+      {
          try
-         {//Inicia el try para controlar excepción IndexOutOfRangeException 
-            if (temp[i] == temp[i + 1])//Condición que verifica los empates
-               emp[i] = 1;//Levanta la bandera del arreglo emp cuando encuentra un empate        
-         }//Cierra el try para controlar excepción IndexOutOfRangeException 
+         {//ESPAÑOL: Inicia el try para controlar excepción IndexOutOfRangeException 
+            if (temp[i] == temp[i + 1])//ESPAÑOL: Condición que verifica los empates
+               emp[i] = 1;//ESPAÑOL: Levanta la bandera del arreglo emp cuando encuentra un empate        
+         }//ESPAÑOL: Cierra el try para controlar excepción IndexOutOfRangeException 
 
          catch (System.IndexOutOfRangeException e)
-         {//Inicia el catch para controlar excepción IndexOutOfRangeException
+         {//ESPAÑOL: Inicia el catch para controlar excepción IndexOutOfRangeException
             System.Console.WriteLine(e.Message);
-         }//Cierra el catch para controlar excepción IndexOutOfRangeException
-      }//Cierra ciclo for de i que recorre todos los valores hasta n
+         }//ESPAÑOL: Cierra el catch para controlar excepción IndexOutOfRangeException
+      }//ESPAÑOL: Cierra ciclo for de i que recorre todos los valores hasta n
 
+
+      //ESPAÑOL: Abre ciclo for de j que recorre todos los valores hasta m
+      //ENGLISH: for loop to check and count the modal value, if doesn't exist a modal value reset the counter   
       for (j = 0; j < m; j++)
-      {//Abre ciclo for de j que recorre todos los valores hasta m
+      {
          if (emp[j] != 0)
-         {//Condición que verifica los empates
-            cont++;//Incrementa el contador de empates
-            emp_acum[j] = cont;//Levanta la bandera del arreglo emp cuando encuentra un empate        
-         }//Cierra if
+         {//ESPAÑOL: Condición que verifica los empates
+            cont++;//ESPAÑOL: Incrementa el contador de empates
+            emp_acum[j] = cont;//ESPAÑOL: Levanta la bandera del arreglo emp cuando encuentra un empate        
+         }//ESPAÑOL: Cierra if
          else//Sino
             cont = 0;//Resetea el contador cuando no hay empates
       }//Cierra ciclo for de j que recorre todos los valores hasta m
 
-      subindice_moda = 0;//Inicializa el subíndice de moda
+
+      subindice_moda = 0;//Inicializa el subíndice de moda | ENGLISH: initialize the modal index
+      //ENGLISH: for loop to check the majority of repetitions and save the modal value and its index
       for (j = 0; j < m; j++)
       {//Abre ciclo for de j que recorre todos los valores hasta m
          if (emp_acum[j] > repeticiones_moda)
@@ -59,7 +91,9 @@ public class moda_JW
             }//Cierra el if simple
       }//Cierra ciclo for de j que recorre todos los valores hasta m
 
-      cont = 0;//Resetea el contador para usarlo nuevamente
+      
+      cont = 0;//Resetea el contador para usarlo nuevamente | ENGLISH: restart the counter again
+      //ENGLISH: for loop to check how often has occurred the modal values
       for (j = 0; j < m; j++)
       {//Abre ciclo for de j que recorre todos los valores hasta m
          if (emp_acum[j] > 0 && emp_acum[j] == repeticiones_moda)
@@ -68,14 +102,20 @@ public class moda_JW
          }//Cierra el if simple
       }//Cierra ciclo for de j que recorre todos los valores hasta m
 
-      sum = 0;//Inicializa el acumulador de suma en cero  
+
+      sum = 0;//Inicializa el acumulador de suma en cero | ENGLISH: initialize the counter woth zero
+      //ENGLISH: for loop to evalue that all values has been different
       for (j = 0; j < m; j++)
       {//Abre ciclo for de j que recorre todos los valores hasta m
          if (emp_acum[j] > 0)//Evalua que los valores de los empates acumulados sean diferentes
             sum++;//Incrementa el acumulador de suma
       }//Cierra ciclo for de j que recorre todos los valores hasta m
+
+      //ENGLISH: print a message when all values are different
       if (sum == 0)//Mensaje que ocurre cuando todos los valores aparecen solo una vez
          sCadenaModa = "Todos los valores ocurren solo una vez.";
+
+      //ENGLISH: pritn a message with the the number of repetitions and the modal value(s)   
       else
       {//Inicia sino
          repeticiones_moda = repeticiones_moda + 1;//Modifica repeticiones_moda para presentar a usuario
